30501c28c54068ab32caab6d6d803bbe872554c5
Mostapha ABDI <abdimostapha@gmail.com>
2020-09-14 00:14:10 +0200

    remove comment

--- src/main/java/com/kata/App.java
+++ src/main/java/com/kata/App.java
@@ -2,10 +2,6 @@ package com.kata;
 
 import java.util.stream.IntStream;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         IntStream.rangeClosed(1, 100).mapToObj(FooBarQix::toFooBarQix).forEach(System.out::println);
